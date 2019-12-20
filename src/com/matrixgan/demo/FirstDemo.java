944e735c702827870d7c3e01bee640556d5caca0
matrixgan <5624969@qq.com>
2019-12-20 16:10:13 +0800

    update code commit

--- src/com/matrixgan/demo/FirstDemo.java
+++ src/com/matrixgan/demo/FirstDemo.java
@@ -2,6 +2,9 @@ package com.matrixgan.demo;
 
 public class FirstDemo {
 	 public static void main(String[] args){
+		int result = 0,x=5,y=10;
+		result = x+ y;
 		System.out.println("hello"); 
-	 }
+		System.out.println(result);
+	}
 }
